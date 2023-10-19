448308b8883fff4f1fad4984ae93bef3d1728387
Ryan <alejandr257@gmail.com>
2023-10-19 14:39:22 -0700

    add query functionality to frontend

--- client/src/components/DisplayData.js
+++ client/src/components/DisplayData.js
@@ -1,23 +1,39 @@
-import React from "react";
-import { useQuery, gql } from "@apollo/client";
+import React, { useState } from "react";
+import { useQuery, useLazyQuery, gql } from "@apollo/client";
 
 const QUERY_ALL_USERS = gql`
     query GetAllUsers {
         users {
             id
             name
-            age
+        }
+    }
+`;
+
+const QUERY_USER = gql`
+    query GetUser($userId: ID!) {
+        user(id: $userId) {
+            name
             username
+            age
         }
     }
 `;
 
 const DisplayData = () => {
+    const [searchedUser, setSearchedUser] = useState("");
     const { data, loading, error } = useQuery(QUERY_ALL_USERS);
+    const [
+        fetchUserData,
+        { data: searchedUserData, error: searchedUserError },
+    ] = useLazyQuery(QUERY_USER);
 
     if (error) {
         console.warn(error);
-        return <div>There was an error</div>;
+    }
+
+    if (searchedUserError) {
+        console.warn(searchedUserError);
     }
 
     return (
@@ -28,9 +44,35 @@ const DisplayData = () => {
                     data.users &&
                     data.users.map((u) => (
                         <div key={u.id}>
-                            {u.id}: {u.name} a.k.a. {u.username}
+                            {u.id}: {u.name}
                         </div>
                     ))}
+            </div>
+
+            <div>
+                <input
+                    type="text"
+                    placeholder="1..."
+                    onChange={(e) => setSearchedUser(e.target.value)}
+                />
+                <button
+                    onClick={() =>
+                        fetchUserData({ variables: { userId: searchedUser } })
+                    }
+                >
+                    Fetch User
+                </button>
+                <div>
+                    {searchedUserData && (
+                        <div>
+                            Name: {searchedUserData.user.name} aka{" "}
+                            {searchedUserData.user.username}
+                        </div>
+                    )}
+                    {searchedUserError && (
+                        <div>There was an error fetching the user</div>
+                    )}
+                </div>
             </div>
         </div>
     );
